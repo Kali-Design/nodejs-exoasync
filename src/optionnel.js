c168bd40d191d2f5645c9f40b9230d27a783e422
Kali-Design <hm_leo@hotmail.com>
2020-09-28 18:12:35 +0200

    maj

--- src/optionnel.js
+++ src/optionnel.js
@@ -1,10 +1,18 @@
 import fsPromises from 'fs/promises'
 import { COPYFILE_EXCL } from 'constants'
 
-const input = 
+const src = process.argv[2]
+const dest = process.argv[3]
 
 fsPromises
     .copyFile('../file1.txt', '../file2.txt', COPYFILE_EXCL)
     
     .then(() => console.log('file1.txt was copied to file2.txt'))
     .catch(() => console.log('The file could not be copied'))
+
+    if (process.argv.length!== 4) {
+        console.log('USAGE: node optionnel.js src dest')
+    }
+    if (!isNaN(src) || (!isNaN(dest))) {
+        console.log('Error : src <string> dest <string>')
+    }
